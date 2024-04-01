71fd3266c8b27cd1e3e14ef05cef9de8a5fbcc10
Dagger Crampton <Crampton05678@gmail.com>
2024-04-01 08:08:15 -0400

    refractor

--- scripts/index.js
+++ scripts/index.js
@@ -27,17 +27,29 @@ const initialCards = [
 
 const profileEditBtn = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
-const profileModalCloseBtn = profileEditModal.querySelector("modal__close");
-const profileTitle = document.querySelector(".profile__title)");
-const profileDescription = document.querySelector(".profile__description)");
+const profileModalCloseBtn = profileEditModal.querySelector(".modal__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
 const profileTitleInput = document.querySelector("#profile-title-input");
 const profileDescriptionInput = document.querySelector("#profile-description-input");
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+
+function closePopop() {
+    profileEditModal.classList.remove("modal__opened");
+}
+
+function handleProfileEditSubmit(e) {
+    e.preventDefault();
+    profileTitle.textContent = profileTitleInput.value;
+    profileDescription.textContent = profileDescriptionInput.value;
+    closePopop();
+}
 
 profileEditBtn.addEventListener("click", () => {
     profileTitleInput.value = profileTitle.textContent;
     profileDescriptionInput.value = profileDescription.textContent;
-    profileEditModal.classList.add("modal");
+    profileEditModal.classList.add("modal__opened");
 });
-profileModalCloseBtn.addEventListener("click", () => {
-    profileEditModal.classList.remove("modal_visible");
-});
+profileModalCloseBtn.addEventListener("click", closePopop);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
