27c89baa349dad34518be07b9fc51a8faffa423e
Dagger Crampton <Crampton05678@gmail.com>
2024-06-11 07:39:49 -0400

    6/11/2024

--- scripts/index.js
+++ scripts/index.js
@@ -2,41 +2,55 @@ const initialCards = [
 {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+    alt: "Yosemite Valley",
 },
 {
     name: "Lake Louise",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+    alt: "Lake Louise",
 },
 {
     name: "Bald Mountains",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+    alt: "Bald Mountains",
 },
 {
     name: "Latemar",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+    alt: "Latemar",
 },
 {
     name: "Vanoise National Park",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+    alt: "Vanoise National Park",
 },
 {
     name: "Lago di Braies",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
-}
+    alt: "Lago di Braies",
+},
 ];
 
 const profileEditBtn = document.querySelector("#profile-edit-button");
+const profileAddBtn = document.querySelector("#profile-add-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
-const profileModalCloseBtn = profileEditModal.querySelector(".modal__close");
+const profileAddModal = document.querySelector("#profile-add-modal");
+const profileEditModalCloseBtn = document.querySelector("#profile-edit-modal-close-button");
+const profileAddModalCloseBtn = document.querySelector("#profile-add-modal-close-button");
 const profileTitle = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
 const profileTitleInput = document.querySelector("#profile-title-input");
 const profileDescriptionInput = document.querySelector("#profile-description-input");
 const profileEditForm = profileEditModal.querySelector(".modal__form");
-const cardTemplate = document.querySelector("#card-template").textContent.firstElementChild;
+const cardTemplate = document.querySelector("#card-template").content;
+const cardsList = document.querySelector(".cardsy__list");
 
-function closePopop() {
-    profileEditModal.classList.remove("modal__opened");
+function closePopup() {
+    profileEditModal.classList.remove("modal_opened");
+}
+
+function closePopup() {
+    profileAddModal.classList.remove("modal_opened");
 }
 
 function getCardElement(cardData) {
@@ -44,6 +58,8 @@ function getCardElement(cardData) {
     const cardImageEl = cardElement.querySelector(".card__image");
     const cardTitleEl = cardElement.querySelector(".card__title");
     cardTitleEl.textContent = cardData.name;
+    cardImageEl.src = cardData.link;
+    cardImageEl.alt = cardData.name;
     return cardElement;
 }
 
@@ -51,20 +67,28 @@ function handleProfileEditSubmit(e) {
     e.preventDefault();
     profileTitle.textContent = profileTitleInput.value;
     profileDescription.textContent = profileDescriptionInput.value;
-    closePopop();
+    closePopup();
 }
 
 profileEditBtn.addEventListener("click", () => {
     profileTitleInput.value = profileTitle.textContent;
     profileDescriptionInput.value = profileDescription.textContent;
-    profileEditModal.classList.add("modal__opened");
+    profileEditModal.classList.add("modal_opened");
 });
-profileModalCloseBtn.addEventListener("click", closePopop);
+
+profileAddBtn.addEventListener("click", () => {
+    profileTitleInput.value = profileTitle.textContent;
+    profileDescriptionInput.value = profileDescription.textContent;
+    profileAddModal.classList.add("modal_opened");
+});
+
+profileEditModalCloseBtn.addEventListener("click", closePopup);
+
+profileAddModalCloseBtn.addEventListener("click", closePopup);
 
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
 
 initialCards.forEach((cardData) => {
     const cardElement = getCardElement(cardData);
-    cardListEl.prepend(cardElement);
+    cardsList.prepend(cardElement);
 });
-console.log(cardTemplate)
